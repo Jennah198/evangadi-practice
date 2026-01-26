b574e8d43e13e1d24bec63d4bcd5e0f638154a16
Nezira <neziraworku198@gmail.com>
2026-01-26 09:04:40 -0800

    practices basic variable, and arthimetic properties

--- Phase 2/Programming Basics/JS/script.js
+++ Phase 2/Programming Basics/JS/script.js
@@ -1,4 +1,5 @@
 alert('your JS file is connected!!')
+// Question 1
 console.log(24 > 3)
 console.log(2 < '12')
 console.log(0 == 2)
@@ -17,3 +18,26 @@ console.log(
     10 > 5 ||
     !('2' == 'two' || 1 == '1'),
 )
+
+// Question 2
+// 1
+// console.log(‘1’ === 1)
+console.log(1 == 1)
+console.log(1 === 1)
+
+//2
+let x = 1 == true
+console.log(x)
+
+// question 3
+let a = 1,
+  b = 2,
+  c
+c = a + b
+console.log(c)
+
+// question 4
+let fname = 'Nezira'
+let lname = 'Worku'
+let fullname = fname + ' ' + lname
+console.log(fullname)
