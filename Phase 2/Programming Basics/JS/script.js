4ed3766052c76b5215dd2eed2c353be5420c618e
Nezira <neziraworku198@gmail.com>
2026-01-26 08:46:40 -0800

    testing basic arthimetic operations

--- Phase 2/Programming Basics/JS/script.js
+++ Phase 2/Programming Basics/JS/script.js
@@ -1,1 +1,19 @@
 alert('your JS file is connected!!')
+console.log(24 > 3)
+console.log(2 < '12')
+console.log(0 == 2)
+console.log(2.0 === 2)
+console.log(2.0 == '2')
+console.log(2 < 'John')
+console.log(2 > 'John')
+console.log('2' < '2')
+console.log('2' > '12')
+console.log(1 == 1 || 3 == 2 || 3 == 7)
+console.log(1 == 1 && 2 == 2 && 3 == 7)
+console.log(1 == 1 || (2 == 2 && 3 == 7))
+console.log(
+  (1 == true && 0 > true) ||
+    '31' > '9' ||
+    10 > 5 ||
+    !('2' == 'two' || 1 == '1'),
+)
