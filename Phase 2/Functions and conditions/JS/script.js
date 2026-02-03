44c10c5a6622fae0403e0db4c34a0db4256eecf2
Nezira <neziraworku198@gmail.com>
2026-02-03 11:40:31 -0800

    add the folder structure for function practce week and worked some questons

---
+++ Phase 2/Functions and conditions/JS/script.js
@@ -0,0 +1,29 @@
+// Question 1
+function myFirst() {
+  console.log('Hello')
+}
+myFirst()
+
+// Question 2
+function mySecond(parameter) {
+  console.log(parameter)
+}
+mySecond('Evangadi')
+
+// Question 3
+function myThird(value) {
+  mySecond(value)
+}
+myThird('hi')
+
+// Question 4
+function myFourth(myArray) {
+  console.log(myArray[0])
+}
+myFourth([1, 2, 3, 4, 5])
+
+// Question 5
+function myFifth(sum) {
+  console.log(sum[0] + sum[1])
+}
+myFifth([5, 3])
