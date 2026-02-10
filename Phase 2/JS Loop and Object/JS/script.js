002977433bc5ef40a0fd3372e4c80430e385b2a1
Nezira <neziraworku198@gmail.com>
2026-02-10 12:11:05 -0800

    added question 16

--- Phase 2/JS Loop and Object/JS/script.js
+++ Phase 2/JS Loop and Object/JS/script.js
@@ -350,3 +350,16 @@ function divisorsOfThree(low, high) {
     }
   }
 }
+
+// Question 16
+// Problem:
+// Classic FizzBuzz from 1 to 100
+
+function fizzBuzz() {
+  for (let i = 1; i <= 100; i++) {
+    if (i % 15 === 0) console.log('FizzBuzz')
+    else if (i % 3 === 0) console.log('Fizz')
+    else if (i % 5 === 0) console.log('Buzz')
+    else console.log(i)
+  }
+}
