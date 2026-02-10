9c80be21dd6922b96385d3f6836ec604ab536814
Nezira <neziraworku198@gmail.com>
2026-02-10 11:57:10 -0800

    worked question 1

---
+++ Phase 2/JS Loop and Object/JS/script.js
@@ -0,0 +1,16 @@
+// Question 1
+// Problem:
+// Print the first 10 integers starting from 1 using a for loop.
+
+// Input Validation:
+// - No input required
+
+// Pseudocode:
+// 1. Loop from 1 to 10
+// 2. Print each number
+
+function printFirstTenIntegers() {
+  for (let i = 1; i <= 10; i++) {
+    console.log(i)
+  }
+}
