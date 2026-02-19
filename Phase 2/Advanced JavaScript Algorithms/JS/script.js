f943fbe0b09e1fa5a345d9e7678545e5abb64ae1
Nezira <neziraworku198@gmail.com>
2026-02-19 11:00:05 -0800

    added question 3

--- Phase 2/Advanced JavaScript Algorithms/JS/script.js
+++ Phase 2/Advanced JavaScript Algorithms/JS/script.js
@@ -40,20 +40,44 @@
 // 3. Compare
 // 4. Print result
 
-function reverseCompare(num) {
-  if (typeof num !== 'number' || num < 10 || num > 99) {
-    console.log('Invalid input: must be a two-digit number')
+// function reverseCompare(num) {
+//   if (typeof num !== 'number' || num < 10 || num > 99) {
+//     console.log('Invalid input: must be a two-digit number')
+//     return
+//   }
+
+//   const reversed = Number(String(num).split('').reverse().join(''))
+
+//   if (num > reversed) {
+//     console.log('Ok')
+//   } else {
+//     console.log('Not ok')
+//   }
+// }
+
+// reverseCompare(72)
+// reverseCompare(23)
+
+// Question 3
+// Return factorial of a positive integer
+
+// PSEUDOCODE:
+// 1. Validate input is non-negative integer
+// 2. If 0 → return 1
+// 3. Multiply numbers from 1 to n
+// 4. Return result
+
+function returnFactorial(n) {
+  if (!Number.isInteger(n) || n < 0) {
+    console.log('Invalid input: must be non-negative integer')
     return
   }
 
-  const reversed = Number(String(num).split('').reverse().join(''))
-
-  if (num > reversed) {
-    console.log('Ok')
-  } else {
-    console.log('Not ok')
-  }
+  if (n === 0) return 1
+  // Recursive case: n! = n * (n-1)!
+  return n * returnFactorial(n - 1)
 }
 
-reverseCompare(72)
-reverseCompare(23)
+console.log(returnFactorial(5))
+console.log(returnFactorial(6))
+console.log(returnFactorial(0))
