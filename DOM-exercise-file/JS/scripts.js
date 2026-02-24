a236b208845c25d861414ea8df6364639ff87896
Nezira <neziraworku198@gmail.com>
2026-02-24 04:46:51 -0800

    question 1 answer

---
+++ DOM-exercise-file/JS/scripts.js
@@ -0,0 +1,8 @@
+// 1.1 Select the element with an id of "sample1"
+const question1 = document.getElementById('sample1')
+
+// 1.2 Print the element itself on the console
+console.log(question1)
+
+// 1.3 Print the content of the element on the console using textContent
+console.log(question1.textContent)
